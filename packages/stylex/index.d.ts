dc2b96eb7f1d57a1201d77d805a51636729df915
Juan Carlos <juancarlos.arias@gmail.com>
2021-05-22 12:46:10 -0700

    Arrange Styles

--- packages/stylex/index.d.ts
+++ packages/stylex/index.d.ts
@@ -1,23 +1,52 @@
 // @author: somibuon, Duc Hoang
 // TODO: need more review
 
-import {CssStyle, Style} from './global';
+import * as css from 'csstype';
+
+type PseudoStyles = {
+  ":first-child"?: css.Properties;
+  ":last-child"?: css.Properties;
+  ":hover"?: css.Properties;
+  ":focus"?: css.Properties;
+  ":selected"?: css.Properties;
+};
+
+type CssStyles = css.Properties & PseudoStyles;
+
+type ClearCSSFields = {
+  [Property in keyof css.Properties]: null;
+};
+export type RemoveUnusedStyles<ApprovedStyles> = {
+  [Property in keyof ClearCSSFields as Exclude<
+    Property,
+    keyof ApprovedStyles
+  >]: null;
+} &
+  ApprovedStyles;
+
+export type Opaque<K, T> = T & {__TYPE__: K};
+export type StyleX$CSS<T> = Opaque<"Styles", RemoveUnusedStyles<T>>;
+export type CompiledStyles<Type extends CssStyles> = {
+  [Property in keyof Omit<Type, keyof PseudoStyles>]: Opaque<"Styles", Type[Property]>;
+};
 
 // tslint:disable-next-line:export-just-namespace
 export = stylex;
 export as namespace stylex;
 
-declare function stylex(...style: (CssStyle | CssStyle[])[]): string;
+declare function stylex<T>(...style: (CompiledStyles<T> | (null | CompiledStyles<T>)[])[]): string;
+
 
 declare namespace stylex {
-  function create<T extends string | number>(styles: Style<T>) : { [key in T]: CssStyle }
+  function create<T extends Record<string, CssStyles>>(styles: T ): CompiledStyles<T>; 
 
-  function dedupe(...styles: CssStyle[]): string;
+  function dedupe(...styles: CssStyles[]): string;
 
   // TODO: maybe remove in next release
-  function compose(...styles: CssStyle[]): CssStyle;
+  function compose(...styles: CssStyles[]): CssStyles;
 
-  function keyframes(rules: CssStyle): string;
+  function keyframes(rules: CssStyles): string;
 
   function inject(cssString: string): void;
 }
+
